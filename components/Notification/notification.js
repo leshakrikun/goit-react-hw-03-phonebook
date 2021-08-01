a3d372381a599b5f15224ae399f621c97af8ced9
leshakrikun <lesha.krikun@gmail.com>
2021-08-01 21:11:21 +0300

    before check

--- components/Notification/notification.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import s from './notification.module.css';
-const Notification  = () => {
-    return (
-            <p className={s.notification} >No feedback given</p>
-    )   
-  }
-  export default Notification;
